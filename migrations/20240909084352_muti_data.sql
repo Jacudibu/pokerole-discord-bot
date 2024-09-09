996d92323b22e9b5e9cf9e5a4a50ab8672b57b47
jacudibu <jacudibu@gmail.com>
2024-09-09 16:14:20 +0200

    Made new DB fields nullable for ze sake of simplicity

--- migrations/20240909084352_muti_data.sql
+++ migrations/20240909084352_muti_data.sql
@@ -2,4 +2,4 @@ ALTER TABLE guild
     ADD COLUMN data_source_id INTEGER;
 
 ALTER TABLE user
-    ADD COLUMN last_data_source_id INTEGER NOT NULL DEFAULT 0;
+    ADD COLUMN last_data_source_id INTEGER;
