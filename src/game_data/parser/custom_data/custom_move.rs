952b4db925c3fdf7646dc293ad0892239492201b
jacudibu <jacudibu@gmail.com>
2024-09-28 09:37:15 +0200

    power and damage are now optional in custom move data

--- src/game_data/parser/custom_data/custom_move.rs
+++ src/game_data/parser/custom_data/custom_move.rs
@@ -5,8 +5,8 @@ use serde::Deserialize;
 pub struct CustomMove {
     pub name: String,
     pub r#type: MoveType,
-    pub power: u8,
-    pub damage: String,
+    pub power: Option<u8>,
+    pub damage: Option<String>,
     pub accuracy: String,
     pub target: String,
     pub effect: String,
